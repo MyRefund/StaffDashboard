320a2cb168045902b28be1e01d9304a0b59fd373
MyRefund <drew@myrefund.co.nz>
2026-02-23 11:04:08 +1300

    Create resources-data.js

---
+++ resources/resources/resources-data.js
@@ -0,0 +1,7 @@
+const RESOURCES = [
+  {
+    title: "Income Confirmation Script",
+    file: "income-confirmation.html",
+    description: "CSR script for confirming income, additional sources, and declaration."
+  }
+];
