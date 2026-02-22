2fd570d4fc44506896f5d33a2671e6d3ba13cdae
MyRefund <drew@myrefund.co.nz>
2026-02-23 11:06:34 +1300

    Delete resources/resources directory

--- resources/resources/resources-data.js
+++
@@ -1,7 +0,0 @@
-const RESOURCES = [
-  {
-    title: "Income Confirmation Script",
-    file: "income-confirmation.html",
-    description: "CSR script for confirming income, additional sources, and declaration."
-  }
-];
